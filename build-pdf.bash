bc1e6e5f9228eed5dacc768f66c50229b055dbf0
Jonatan Jönsson <jontejj@gmail.com>
2019-05-06 08:36:44 +0200

    Preliminary
    TODO: format

--- build-pdf.bash
+++ build-pdf.bash
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+#Install with brew cask install basictex
+#sudo tlmgr install helvetic
 export PATH=$PATH:/Library/TeX/Distributions/.DefaultTeX/Contents/Programs/i386/
 pdflatex Jonatan_Jonsson_CV.tex
